ce7744146d16540079867643f7eddcb1d7f02014
Arian <arian@aryweb.nl>
2010-12-24 17:19:08 +0100

    Add the possibility to set the default Socket and Domain for every instance of DA_Api with static properties

--- Source/DA/Api.php
+++ Source/DA/Api.php
@@ -16,13 +16,33 @@ abstract class DA_Api {
 	protected $domain;
 
 	/**
+	 * an instance of HTTPSocket which can be set to default, so you don't have to pass this into every DA_Api instance
+	 * @var HTTPSocket
+	 */
+	static public $DEFAULT_SOCKET;
+
+	/**
+	 * The default domain
+	 * @var string
+	 */
+	static public $DEFAULT_DOMAIN;
+
+	/**
 	 *
 	 * @param HTTPSocket $sock
 	 * @return
 	 */
-	public function __construct(HTTPSocket $sock, $domain = null){
-		$this->sock = $sock;
-		$this->domain = $domain;
+	public function __construct($sock = null, $domain = null){
+
+		$this->sock = self::$DEFAULT_SOCKET;
+		if ($sock instanceof HTTPSocket) $this->sock = $sock;
+		if (!($this->sock instanceof HTTPSocket)){
+			throw new DA_Exception('The socket is not an instance of HTTPSocket, set the first argument or the DA_Api::$DEFAULT_SOCKET variables');
+		}
+
+		$this->domain = self::$DEFAULT_DOMAIN;
+		if ($domain !== null) $this->domain = $domain;
+
 	}
 
 	public function setDomain($domain){
